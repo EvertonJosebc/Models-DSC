669815efc00f54c965a63f4a7b7f4f300463c6ad
EvertonJosebc <everton.jose@escolar.ifrn.edu.br>
2022-10-26 17:12:47 -0300

    First Commit

---
+++ main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from main.models import *
+
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Filme)
+admin.site.register(Locacao)
+
+# Register your models here.
